5ff20c4f486e2d67dac5197e666252bead058033
Maidehua <94219919+Maidehua@users.noreply.github.com>
2026-09-17 17:19:17 -0700

    add windows/LanDrop.Windows/FileSender.cs

---
+++ windows/LanDrop.Windows/FileSender.cs
@@ -0,0 +1,7 @@
+using System.Net.Http.Headers;
+namespace LanDrop.Windows;
+public static class FileSender
+{
+    private static readonly HttpClient Client = new() { Timeout = TimeSpan.FromHours(1) };
+    public static async Task SendAsync(Peer peer, string path) { await using var stream=File.OpenRead(path); using var form=new MultipartFormDataContent(); using var part=new StreamContent(stream); part.Headers.ContentType=new MediaTypeHeaderValue("application/octet-stream"); form.Add(part,"file",Path.GetFileName(path)); using var response=await Client.PostAsync($"http://{peer.Address}:{peer.Port}/api/files",form); response.EnsureSuccessStatusCode(); }
+}
